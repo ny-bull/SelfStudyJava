bb690d70b6ba0c652c364570a7ecf3d873dafdc0
nyuta <nyuta.main@gmail.com>
2024-03-24 17:54:34 +0900

    Chapter02

---
+++ src/to/msn/wings/selfstudyjava/chap02/Check.java
@@ -0,0 +1,20 @@
+package to.msn.wings.selfstudyjava.chap02;
+
+public class Check {
+    public static void main(String[] args) {
+        var value = (double)10;
+        var helloWorld = """
+               ようこそ
+               Javaの世界へ
+               """;
+        System.err.println(helloWorld); 
+        String str = null;
+        int[][] data = new int[5][4];
+        
+        var list = new int[][] {
+            {2, 3, 5},
+            {1, 2},
+            {10, 11, 12, 13},
+        };
+    }
+}
